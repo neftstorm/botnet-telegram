fe1b3529411906c8df5ad043c65f29fd834f8fba
neftstorm <neftstorm@gmail.com>
2024-09-18 17:04:35 +0700

    botnet-telegram

---
+++ settings.js
@@ -0,0 +1,7 @@
+const settings = {
+  ownerName: 'Neft', // Ganti dengan nama owner lu
+  botToken: '7489603153:AAHMZecGT2gzE3Fgi3LgItM2tKu_gHFMWws', // Ganti dengan token bot Telegram lu
+  owner: '6853349771', //OWNER user id
+};
+
+module.exports = settings;
